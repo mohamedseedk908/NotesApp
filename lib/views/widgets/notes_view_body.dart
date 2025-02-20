cb039e4804da82c5f4d18462bbe3f969a133b8f2
Mohamed Seedyk <mohamedseedk908@gmail.com>
2025-02-19 17:51:46 -0800

    Create Notes List View

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:notes/views/widgets/create_custom_notes_item.dart';
 import 'package:notes/views/widgets/custom_app_bar.dart';
+import 'package:notes/views/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -14,65 +16,11 @@ class NotesViewBody extends StatelessWidget {
             height: 50,
           ),
           CustomAppBar(),
-          NotesItem(),
+          Expanded(child: NotesListView()),
         ],
       ),
     );
   }
 }
 
-class NotesItem extends StatelessWidget {
-  const NotesItem({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(
-        top: 24,
-        bottom: 24,
-        left: 16,
-      ),
-      decoration: BoxDecoration(
-        color: Colors.yellow,
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Padding(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: Text(
-                "Flutter Developer",
-                style: TextStyle(color: Colors.black, fontSize: 26),
-              ),
-            ),
-            subtitle: Text(
-              "i Am Muhammad Sadiq Abdulhadi  A Flutter developer && backEnd Developer ",
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.8),
-                fontSize: 20,
-              ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.delete,
-                color: Colors.black,
-                size: 35,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text("May21, 2025",
-                style: TextStyle(
-                  color: Colors.black.withOpacity(0.8),
-                  fontSize: 20,
-                )),
-          ),
-        ],
-      ),
-    );
-  }
-}
