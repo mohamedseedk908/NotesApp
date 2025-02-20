30eea6df46e9ac1c8f81c618c074a4c0159be268
Mohamed Seedyk <mohamedseedk908@gmail.com>
2025-02-19 21:32:46 -0800

    Add BorderRadius In Button Sheet

--- lib/views/widgets/add_notes_button_sheet.dart
+++ lib/views/widgets/add_notes_button_sheet.dart
@@ -5,12 +5,6 @@ class AddNotesButtonSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-
-        ],
-      ),
-    );
+    return Container();
   }
 }
