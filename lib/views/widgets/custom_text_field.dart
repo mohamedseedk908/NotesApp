e9e3fe79e6f0874d2b5ef8fb9cacdfddb85b25d3
Mohamed Seedyk <mohamedseedk908@gmail.com>
2025-02-19 22:11:53 -0800

    Add Field to Notes Button Sheet

--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -2,28 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:notes/views/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
-
+  const CustomTextField({super.key, required this.hint,this.maxLines = 1 });
+final String hint;
+final int maxLines;
   @override
   Widget build(BuildContext context) {
     return TextField(
-      cursorColor:ConstantsColors.kPrimaryColors,
-       decoration: InputDecoration(
-         hintText: "Title",
-         hintStyle: TextStyle(color: ConstantsColors.kPrimaryColors,),
-         border: buildBorder(),
-         enabledBorder: buildBorder(),
-         focusedBorder: buildBorder(ConstantsColors.kPrimaryColors,),
-       ),
+      cursorColor: ConstantsColors.kPrimaryColors,
+      maxLines: maxLines,
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: TextStyle(
+          color: ConstantsColors.kPrimaryColors,
+        ),
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(
+          ConstantsColors.kPrimaryColors,
+        ),
+      ),
     );
   }
 }
 
-OutlineInputBorder buildBorder([color]){
-  return  OutlineInputBorder(
+OutlineInputBorder buildBorder([color]) {
+  return OutlineInputBorder(
     borderRadius: BorderRadius.circular(16),
     borderSide: BorderSide(
-        color: color ?? Colors.white,
+      color: color ?? Colors.white,
     ),
   );
 }
