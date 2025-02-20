549f8e8b86d9af8b4cad06cb5896fc7f51da2257
Mohamed Seedyk <mohamedseedk908@gmail.com>
2025-02-19 17:28:56 -0800

    Create Custom Note Item

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -9,16 +9,17 @@ class NotesViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24.0),
       child: Column(
-          children: [
-            SizedBox(height: 50 ,),
-            CustomAppBar(),
-          ],
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(),
+          NotesItem(),
+        ],
       ),
     );
   }
 }
-
-
 
 class NotesItem extends StatelessWidget {
   const NotesItem({super.key});
@@ -26,20 +27,52 @@ class NotesItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.yellow,
+      padding: EdgeInsets.only(
+        top: 24,
+        bottom: 24,
+        left: 16,
+      ),
       decoration: BoxDecoration(
+        color: Colors.yellow,
         borderRadius: BorderRadius.circular(15),
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           ListTile(
-            title:Text("Flutter Developer") ,
-            subtitle: Text("i Am Muhammad Sadiq Abdulhadi  A Flutter developer && backEnd Developer "),
-            trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete)),
-          )
+            title: Padding(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: Text(
+                "Flutter Developer",
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+            ),
+            subtitle: Text(
+              "i Am Muhammad Sadiq Abdulhadi  A Flutter developer && backEnd Developer ",
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.8),
+                fontSize: 20,
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.delete,
+                color: Colors.black,
+                size: 35,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text("May21, 2025",
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.8),
+                  fontSize: 20,
+                )),
+          ),
         ],
       ),
     );
   }
 }
-
