1117efa81cb9ce5257d22a2c5e51df6cc3f3e62a
Mohamed Seedyk <mohamedseedk908@gmail.com>
2025-02-22 13:59:19 -0800

    create addote ethod

--- lib/add_notes_cubits/add_notes_cubit.dart
+++ lib/add_notes_cubits/add_notes_cubit.dart
@@ -1,14 +1,21 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes/model/note_model.dart';
-
+import 'package:notes/views/constants.dart';
 part 'add_notes_state.dart';
 
 class AddNotesCubit extends Cubit<AddNotesState> {
   AddNotesCubit() : super(AddNotesInitial());
 
-  addNotes(NoteModel notes)
-  {
-
+  addNotes(NoteModel notes) async{
+    emit(AddNotesLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(Constants.kNotesBox);
+      emit(AddNotesSuccess());
+      await notesBox.add(notes);
+    } on Exception catch (e) {
+      emit(AddNotesFailure(e.toString()));
+    }
   }
 }
