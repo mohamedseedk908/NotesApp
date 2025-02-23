103d261a89b402b6aeb7abef06ed41bcb2de6734
Mohamed Seedyk <mohamedseedk908@gmail.com>
2025-02-23 08:47:10 -0800

    add notes state

--- lib/cubits/add_notes_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'add_notes_state.dart';
-
-class AddNotesCubit extends Cubit<AddNotesState> {
-  AddNotesCubit() : super(AddNotesInitial());
-}
--- lib/cubits/add_notes_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'add_notes_cubit.dart';
-
-@immutable
-sealed class AddNotesState {}
-
-final class AddNotesInitial extends AddNotesState {}
